6ad11ea28f8b2ddc9994747f4ea08b2c648047c8
Angad Singh <angadsingh567890@gmail.com>
2018-12-02 17:27:55 +0530

    Fix favicon

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import '../less/main.less'
+import Icon from '../img/hyper.png'
 
 let settingsOpen = false
 
@@ -9,6 +10,15 @@ function toggleSettings() {
 }
 
 document.addEventListener('DOMContentLoaded', e => {
+  // Set Favicon
+  const link =
+    document.querySelector("link[rel*='icon']") ||
+    document.createElement('link')
+  link.type = 'image/png'
+  link.rel = 'icon'
+  link.href = Icon
+
+  document.querySelector('head').appendChild(link)
   document.querySelector('.settings .toggle').addEventListener('click', e => {
     e.preventDefault()
     toggleSettings()
